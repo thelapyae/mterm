59080e14150cd7cdd9c508853a2ae824f0ac27a7
La Pyae <thelapyae@icloud.com>
2026-04-25 16:55:53 +0630

    Cleanup: Convert to release-only repository (Source removed)

--- preload.js
+++
@@ -1,9 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('termAPI', {
-    spawnPTY: (size) => ipcRenderer.send('spawn-pty', size),
-    sendInput: (data) => ipcRenderer.send('pty-input', data),
-    sendResize: (cols, rows) => ipcRenderer.send('pty-resize', { cols, rows }),
-    onOutput: (callback) => ipcRenderer.on('pty-output', (_, data) => callback(data)),
-    log: (msg) => ipcRenderer.send('renderer-log', msg)
-});
--- test-burmese.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "--- Burmese Text Rendering Test ---"
-echo "Hello: မင်္ဂလာပါ"
-echo "Burmese: မြန်မာဘာသာ"
-echo "Complex shaping test (stacked glyphs): ကမ္ဘာကြီး"
-echo "Numbers: ၁ ၂ ၃ ၄ ၅ ၆ ၇ ၈ ၉ ၀"
-echo "----------------------------------"
